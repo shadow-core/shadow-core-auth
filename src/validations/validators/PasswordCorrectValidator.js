2386da60c74a9a3dd896857a495485d503d637df
Gray Hound <admin@devilmaydie.name>
2019-08-07 15:58:39 +0500

    PasswordCorrectValidator checks that value exists and not empty

--- src/validations/validators/PasswordCorrectValidator.js
+++ src/validations/validators/PasswordCorrectValidator.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 
 export default function PasswordCorrectValidator(validation) {
   return ((value) => {
-    if (!validation.user) {
+    if (!validation.user && value) {
       return true;
     }
     return bcrypt.compareSync(value, validation.user.passwordHash);
